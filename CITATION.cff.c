659d0ba38d6e49b6c1d3a3cb7a40caec59c9ce8c
Charles Nye <nye@10-249-244-108.wireless.oregonstate.edu>
2026-01-12 09:36:01 -0800

    Added citation documentation.

---
+++ CITATION.cff.c
@@ -0,0 +1,12 @@
+cff-version: 1.2.0
+message: "If you use any material from this repository, please cite it as below."
+authors:
+  - family-names: Nye
+    given-names: Charles
+    orcid: https://orcid.org/0009-0009-5893-2891
+title: "Novel observations of Earth's ultimate hadal zone: Passive Acoustic records and eDNA samples from Challenger Deep, Mariana Trench"
+version: 1.0.0
+identifiers:
+  - type: doi
+    value: 10.5281/zenodo.18224631
+date-released: 2026-01-12
